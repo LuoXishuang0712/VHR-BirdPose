a37683a58188a03f6b9101976beccb80f33dd699
LuoXishuang0712 <hra20020620h@outlook.com>
2023-08-31 18:07:32 +0800

    commit codes

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
 # VHR-BirdPose
-**ALL THE CODE WILL BE RELEASED AFTER THE PAPER ACCEPTED.**
+
+About this repo: [paper](https://doi.org/10.3390/electronics12173643) | [HRNet_readme](./README_HRNet.md)
+
+What we use: [HRNet](https://github.com/leoxiaobin/deep-high-resolution-net.pytorch) | [Animal Kingdom](https://github.com/sutdcv/Animal-Kingdom) | [ViTPose](https://github.com/ViTAE-Transformer/ViTPose) | [Timm](https://github.com/huggingface/pytorch-image-models)
+
+## Environment
+
+This code has been validated on:
+* NVIDIA Geforce RTX 3090Ti (CUDA11.0, PyTorch1.7.0+cu110, Ubuntu20.04)
+* NVIDIA Tesla V100-PCIE 32GB (CUDA10.2, PyTorch1.10.0, Ubuntu18.04)
+
+## Installation
+
+1. Require a copy of Animal Kingdom and prepare the data alone with the code according to [the readme of pose estimation task](https://github.com/sutdcv/Animal-Kingdom/blob/master/Animal_Kingdom/pose_estimation/README_pose_estimation.md), you can stop after finish the Step3 of the section "Instructions to run Pose Estimation models".
+1. Clone this project and execute `cp $OUR_REPO/lib/models/pose_vhr.py $OUR_REPO/lib/models/cross_attn.py $OUR_REPO/lib/models/vit.py $OUR_REPO/lib/models/base_backbone.py $AK_PE/code/hrnet/lib/models/`, `cp -r $OUR_REPO/experiments/mpii/vhrbirdpose $AK_PE/code/hrnet/experiments/mpii/`, and `cp -f $OUR_REPO/lib/utils/utils.py $AK_PE/code/hrnet/lib/utils/`, you may need specified the paths to out reporistory and pose estimation folder of Animal Kingdom by executing `export OUT_REPO={PATH TO THIS PROJECT}` and `export AK_PE={PATH TO POSE ESTIMATION}`.
+    > For Windows, just simply copy the lib/models and experiments/mpii/vhr to the appearently same place in the %ANIMAL_KINGDOM_ROOT%/pose_estimation/code/hrnet by using GUI or use PowerShell/Cygwin or others posix compact shell to execute the shell code above.
+1. Append `import models.pose_vhr` to the end of the file `$AK_PE%/lib/models/__init__.py`.
+1. Install [Timm](https://github.com/huggingface/pytorch-image-models)==0.4.9 and einops by `python -m pip install timm==0.4.9 einops`
+
+## Testing
+
+Change current diectory to `$AK_PE$/code/hrnet`, run `python tools/train.py --cfg experiments/mpii/vhrbirdpose/w32_256x256_adam_lr1e-3_ak_vhr_b.yaml`.
+
+## Pretrained
+
+The pretrained weight can be download from [Google Drive]() | [Baidu Netdisk]()
+
+## Citing
+
+```
+@Article{
+    he2023vhrbirdpose,
+    AUTHOR = {He, Runang and Wang, Xiaomin and Chen, Huazhen and Liu, Chang},
+    TITLE = {VHR-BirdPose: Vision Transformer-Based HRNet for Bird Pose Estimation with Attention Mechanism},
+    JOURNAL = {Electronics},
+    VOLUME = {12},
+    YEAR = {2023},
+    NUMBER = {17},
+    ARTICLE-NUMBER = {3643},
+}
+```
---
+++ lib/models/cross_attn.py
@@ -0,0 +1,165 @@
+r"""Cross Attention Module
+
+Modified by [Timm](https://github.com/huggingface/pytorch-image-models) at #927f031
+@misc{rw2019timm,
+  author = {Ross Wightman},
+  title = {PyTorch Image Models},
+  year = {2019},
+  publisher = {GitHub},
+  journal = {GitHub repository},
+  doi = {10.5281/zenodo.4414861},
+  howpublished = {\url{https://github.com/rwightman/pytorch-image-models}}
+}
+
+Original file head will be attached before the code
+"""
+
+# timm/layers/drop.py
+""" DropBlock, DropPath
+
+PyTorch implementations of DropBlock and DropPath (Stochastic Depth) regularization layers.
+
+Papers:
+DropBlock: A regularization method for convolutional networks (https://arxiv.org/abs/1810.12890)
+
+Deep Networks with Stochastic Depth (https://arxiv.org/abs/1603.09382)
+
+Code:
+DropBlock impl inspired by two Tensorflow impl that I liked:
+ - https://github.com/tensorflow/tpu/blob/master/models/official/resnet/resnet_model.py#L74
+ - https://github.com/clovaai/assembled-cnn/blob/master/nets/blocks.py
+
+Hacked together by / Copyright 2020 Ross Wightman
+"""
+import torch.nn as nn
+
+def drop_path(x, drop_prob: float = 0., training: bool = False, scale_by_keep: bool = True):
+    """Drop paths (Stochastic Depth) per sample (when applied in main path of residual blocks).
+
+    This is the same as the DropConnect impl I created for EfficientNet, etc networks, however,
+    the original name is misleading as 'Drop Connect' is a different form of dropout in a separate paper...
+    See discussion: https://github.com/tensorflow/tpu/issues/494#issuecomment-532968956 ... I've opted for
+    changing the layer and argument names to 'drop path' rather than mix DropConnect as a layer name and use
+    'survival rate' as the argument.
+
+    """
+    if drop_prob == 0. or not training:
+        return x
+    keep_prob = 1 - drop_prob
+    shape = (x.shape[0],) + (1,) * (x.ndim - 1)  # work with diff dim tensors, not just 2D ConvNets
+    random_tensor = x.new_empty(shape).bernoulli_(keep_prob)
+    if keep_prob > 0.0 and scale_by_keep:
+        random_tensor.div_(keep_prob)
+    return x * random_tensor
+
+
+class DropPath(nn.Module):
+    """Drop paths (Stochastic Depth) per sample  (when applied in main path of residual blocks).
+    """
+    def __init__(self, drop_prob: float = 0., scale_by_keep: bool = True):
+        super(DropPath, self).__init__()
+        self.drop_prob = drop_prob
+        self.scale_by_keep = scale_by_keep
+
+    def forward(self, x):
+        return drop_path(x, self.drop_prob, self.training, self.scale_by_keep)
+
+    def extra_repr(self):
+        return f'drop_prob={round(self.drop_prob,3):0.3f}'
+
+class CrossAttention(nn.Module):
+    def __init__(
+            self,
+            dim,
+            num_heads=8,
+            qkv_bias=False,
+            attn_drop=0.,
+            proj_drop=0.,
+    ):
+        super().__init__()
+        self.num_heads = num_heads
+        head_dim = dim // num_heads
+        # NOTE scale factor was wrong in my original version, can set manually to be compat with prev weights
+        self.scale = head_dim ** -0.5
+
+        self.wq = nn.Linear(dim, dim, bias=qkv_bias)
+        self.wk = nn.Linear(dim, dim, bias=qkv_bias)
+        self.wv = nn.Linear(dim, dim, bias=qkv_bias)
+        self.attn_drop = nn.Dropout(attn_drop)
+        self.proj = nn.Linear(dim, dim)
+        self.proj_drop = nn.Dropout(proj_drop)
+
+    def forward(self, x):
+        B, N, C = x.shape
+        # B1C -> B1H(C/H) -> BH1(C/H)
+        q = self.wq(x[:, 0:1, ...]).reshape(B, 1, self.num_heads, C // self.num_heads).permute(0, 2, 1, 3)
+        # BNC -> BNH(C/H) -> BHN(C/H)
+        k = self.wk(x).reshape(B, N, self.num_heads, C // self.num_heads).permute(0, 2, 1, 3)
+        # BNC -> BNH(C/H) -> BHN(C/H)
+        v = self.wv(x).reshape(B, N, self.num_heads, C // self.num_heads).permute(0, 2, 1, 3)
+
+        attn = (q @ k.transpose(-2, -1)) * self.scale  # BH1(C/H) @ BH(C/H)N -> BH1N
+        attn = attn.softmax(dim=-1)
+        attn = self.attn_drop(attn)
+
+        x = (attn @ v).transpose(1, 2).reshape(B, 1, C)  # (BH1N @ BHN(C/H)) -> BH1(C/H) -> B1H(C/H) -> B1C
+        x = self.proj(x)
+        x = self.proj_drop(x)
+        return x
+
+# timm/models/crossvit.py
+""" CrossViT Model
+
+@inproceedings{
+    chen2021crossvit,
+    title={{CrossViT: Cross-Attention Multi-Scale Vision Transformer for Image Classification}},
+    author={Chun-Fu (Richard) Chen and Quanfu Fan and Rameswar Panda},
+    booktitle={International Conference on Computer Vision (ICCV)},
+    year={2021}
+}
+
+Paper link: https://arxiv.org/abs/2103.14899
+Original code: https://github.com/IBM/CrossViT/blob/main/models/crossvit.py
+
+NOTE: model names have been renamed from originals to represent actual input res all *_224 -> *_240 and *_384 -> *_408
+
+Modifications and additions for timm hacked together by / Copyright 2021, Ross Wightman
+"""
+
+# Copyright IBM All Rights Reserved.
+# SPDX-License-Identifier: Apache-2.0
+
+
+"""
+Modifed from Timm. https://github.com/rwightman/pytorch-image-models/blob/master/timm/models/vision_transformer.py
+
+"""
+class CrossAttentionBlock(nn.Module):
+
+    def __init__(
+            self,
+            dim,
+            num_heads,
+            mlp_ratio=4.,
+            qkv_bias=False,
+            proj_drop=0.,
+            attn_drop=0.,
+            drop_path=0.,
+            act_layer=nn.GELU,
+            norm_layer=nn.LayerNorm,
+    ):
+        super().__init__()
+        self.norm1 = norm_layer(dim)
+        self.attn = CrossAttention(
+            dim,
+            num_heads=num_heads,
+            qkv_bias=qkv_bias,
+            attn_drop=attn_drop,
+            proj_drop=proj_drop,
+        )
+        # NOTE: drop path for stochastic depth, we shall see if this is better than dropout here
+        self.drop_path = DropPath(drop_path) if drop_path > 0. else nn.Identity()
+
+    def forward(self, x):
+        x = x[:, 0:1, ...] + self.drop_path(self.attn(self.norm1(x)))
+        return x
